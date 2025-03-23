ea49e38fcf54703e973eece82bc0f4e9d4287909
HenrySchall <henrique.schall@outlook.com>
2025-03-23 15:14:05 -0300

    .

--- Machine Learning/Supervised Learning/Classification.py
+++ Machine Learning/Supervised Learning/Classification.py
@@ -11,11 +11,41 @@ import plotly.express as px
 ### Introduction ###
 ####################
 
+# Retiado de: https://www.kaggle.com/datasets/laotse/credit-risk-dataset
 dados = "https://drive.google.com/uc?export=download&id=1wMapByTvMFt16zz9Bd2643eTHJXtEhnX"
 df = pd.read_csv(dados)
 
 df
 df.describe()
 
-df[df['income'] >= 69995.685578]
-df[df['loan'] <= 1.377630]
+# pagou o empréstimo
+# Não pagou o empréstimo
+
+max_val = df["income"].max()
+print(max_val)
+
+min_val = df["loan"].min()
+print(min_val)
+
+df[df["income"] >= 69995.6855783239]
+df[df["loan"] <= 1.4000000]
+
+plt.hist(x = df['age'])
+plt.title("Distribuição dos Valores")
+plt.show()
+
+sns.countplot(x = df['default'])
+plt.title("Distribuição dos Valores")
+plt.show()
+
+grafico = px.scatter_matrix(df, dimensions=['age', 'income', 'loan'], color = 'default')
+grafico.show()
+
+##################
+### Tratamento ###
+##################
+
+df.loc[df['age'] < 0]
+
+base_credit2 = df.drop('age', axis = 1)
+base_credit2
