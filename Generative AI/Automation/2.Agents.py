d87e2d298dbbd470945f9d61deeb4821e4af4a2a
HenrySchall <henrique.schall@outlook.com>
2025-09-01 22:17:23 -0300

    .

---
+++ Generative AI/Automation/2.Agents.py
@@ -0,0 +1,65 @@
+########################
+### Install Packages ###
+########################
+
+import subprocess
+import sys
+
+def install_packages(pacotes):
+    for pacote in pacotes:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", pacote])
+
+# List of packages
+packages_list = ["numpy", "pandas", "matplotlib", "scipy", "seaborn","statsmodels", "plotly", "gurobipy",
+"yfinance", "scikit-learn", "panel", "datashader", "param", "colorcet", "transformers","einops","accelerate", 
+"bitsandbytes"]
+
+install_packages(packages_list)
+
+# pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128
+# print(torch.__version__)
+# pip install cuda-python==12.8
+# print(torch.version.cuda)
+
+# Links 
+# - https://docs.nvidia.com/deeplearning/cudnn/backend/latest/reference/support-matrix.html#f1
+# - https://pytorch.org/get-started/locally/
+
+#####################
+### Load Packages ###
+#####################
+
+import gurobipy as gp
+import pandas as pd 
+import seaborn as sns
+import plotly.express as px
+import numpy as np
+import panel as pn 
+import seaborn.objects as so
+import matplotlib as mpl
+import colorcet as cc
+import matplotlib.pyplot as plt
+import math
+import datetime
+import gc
+import param
+import sklearn
+import scipy
+import string
+import random
+import torch
+import os
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline, BitsAndBytesConfig
+
+#####################
+### Configuration ###
+#####################
+
+# My License Keys
+# https://drive.google.com/file/d/1Cu5iQHkbF47FLSXpTH-l75ArX9KBDByd/view?usp=sharing
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('Using device:', device)
+
+torch.random.manual_seed(20)
+os.environ["HF_TOKEN"] = "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"
