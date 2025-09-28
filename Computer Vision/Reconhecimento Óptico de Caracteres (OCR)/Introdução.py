d8a1b41bc4862352197496cba543375ab1d49423
Henrique Schall <henrique.schall@outlook.com>
2025-09-27 21:53:07 -0300

    .

--- Computer Vision/Reconhecimento Óptico de Caracteres (OCR)/Introdução.py
+++ Computer Vision/Reconhecimento Óptico de Caracteres (OCR)/Introdução.py
@@ -1,18 +1,22 @@
-# Windows: winget install -e --id UB-Mannheim.TesseractOCR
-# MacOS: brew install tesseract
-# Linux: sudo apt install tesseract-ocr
+# Install Tesseract OCR
+#   Windows: winget install -e --id UB-Mannheim.TesseractOCR
+#   MacOS: brew install tesseract
+#   Linux: sudo apt install tesseract-ocr
 
 # pip install pytesseract
 # pip install pillow
 # pip install opencv-python
-
+ 
 import pytesseract
 import numpy as np
 import cv2
 import requests
-from PIL import Image
 
-url = "https://raw.githubusercontent.com/HenrySchall/Databases/main/Artificial%20Intelligence/Computer%20Vision/Reconhecimento%20Óptico%20de%20Caracteres%20(OCR)/Imagens/teste01.jpg"
+################
+### Imagem 1 ###
+################
+
+url = "https://raw.githubusercontent.com/HenrySchall/Databases/main/Artificial%20Intelligence/Computer%20Vision/Reconhecimento%20Óptico%20de%20Caracteres%20(OCR)/Imagens/teste02.jpg"
 
 # Baixar a imagem como bytes
 response = requests.get(url)
@@ -34,5 +38,26 @@ cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 pytesseract.pytesseract.tesseract_cmd = "C:/Program Files/Tesseract-OCR/tesseract.exe"
+texto = pytesseract.image_to_string(rgb)
+print(texto)
+
+################
+### Imagem 2 ###
+################
+
+url2 = "https://raw.githubusercontent.com/HenrySchall/Databases/main/Artificial%20Intelligence/Computer%20Vision/Reconhecimento%20Óptico%20de%20Caracteres%20(OCR)/Imagens/teste02.jpg"
+
+response = requests.get(url2)
+response.raise_for_status()
+
+img_array = np.frombuffer(response.content, np.uint8)
+img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+
+rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+cv2.imshow("Imagem", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 texto = pytesseract.image_to_string(rgb)
 print(texto)
